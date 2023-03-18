211746e02623fc63d11d8de3a46f61c4b8b630c2
etanguy <etanguy@student.42.fr>
2023-03-18 14:52:29 +0100

    gateante de montagne

--- command_line.c
+++ command_line.c
@@ -12,6 +12,47 @@
 
 #include "pipex.h"
 
+int	how_many_child(char *path)
+{
+	int	i;
+	int	nb_child;
+
+	i = 0;
+	nb_child = 0;
+	while (path[i])
+	{
+		if (path[i] == '/')
+			nb_child++;
+		i++;
+	}
+	return (nb_child);
+}
+
+int	change_path_value(int *pathl, char **split, char ***new_split, char **pthtc)
+{
+	int	i;
+
+	i = 0;
+	(*pathl) = how_many_child(split[0]);
+	if ((*pathl) > 0)
+	{
+		(*pthtc) = ft_strdup(split[0]);
+		if (!(*pthtc))
+			return (0);
+		(*new_split) = ft_split(split[0], '/');
+		if (!(*new_split))
+			return (0);
+		free(split[0]);
+		while ((*new_split)[i + 1])
+			i++;
+		split[0] = ft_strdup((*new_split)[i]);
+		if (!(split[0]))
+			return (free(*pthtc), 0);
+		ft_free_dbchar_tab((*new_split), 0);
+	}
+	return (1);
+}
+
 t_command	make_command(char *str, t_data *data)
 {
 	t_command	res;
@@ -43,6 +84,33 @@ void swap_first_param(t_data *data)
 	data->command_line[1] = tmp;
 }
 
+void	need_creat_file(char *folder_name)
+{
+	int			fd;
+	struct stat	sb;
+
+	sb.st_mode = 0;
+	stat(folder_name, &sb);
+	if (!access(folder_name, F_OK) == 0)
+	{
+		fd = open(folder_name, O_RDWR | O_CREAT, S_IRUSR
+			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+		close(fd);
+	}
+}
+
+int	check_file_output(int i, int j, char **split, t_command *command_line)
+{
+	change_path_value()
+	need_creat_file(split[i]);
+	if (command_line[j].path)
+		free(command_line[j].path);
+	command_line[j].path = ft_strdup(split[i]);
+	if (!command_line[j].path)
+		return (0);
+	return (1);
+}
+
 int	make_command_line(int argc, char **argv, t_data *data)
 {
 	int	i;
@@ -51,6 +119,8 @@ int	make_command_line(int argc, char **argv, t_data *data)
 	i = 1;
 	j = 0;
 	data->command_line = malloc(sizeof(t_command) * (argc));
+	data->command_line[argc - 1].path = NULL;
+	data->command_line[0].path = NULL;
 	while (i < argc)
 	{
 		data->command_line[j] = make_command(argv[i], data);
